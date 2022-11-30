1394b8529c4b99b26fb9f784b35909c751d96bd3
drako9159 <drako9159@gmail.com>
2022-11-29 19:43:50 -0600

    add function button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,18 @@ import { useState } from "react";
 
 export default function Header({ color, setColor }) {
   const [night, setNight] = useState(true);
+  const [btn, setBtn] = useState(styles.notActive);
+  const [nav, setNav] = useState(styles.navNone);
+
+  function handleClickBurguer(e) {
+    if (btn === styles.notActive) {
+      setNav(styles.burguerNavBlack);
+      setBtn(styles.active);
+    } else {
+      setNav(styles.navNone);
+      setBtn(styles.notActive);
+    }
+  }
 
   function handleClick() {
     if (color === "black") {
@@ -33,28 +45,53 @@ export default function Header({ color, setColor }) {
         </ul>
       </div>
       <div className={styles.buttons}>
-        <div
-          className={color === "black" ? styles.dayOrNight : styles.Night}
-          onClick={handleClick}
-        >
-          <img
-            className={
-              color === "white" ? styles.pickDay : styles.pickDayAnimation
-            }
-            src={pickDay}
-          ></img>
-          <img
-            className={
-              color === "black" ? styles.pickNight : styles.pickNightAnimation
-            }
-            src={pickNight}
-          ></img>
+        <div className={styles.iconDayOrNigth}>
+          <div
+            className={color === "black" ? styles.dayOrNight : styles.Night}
+            onClick={handleClick}
+          >
+            <img
+              className={
+                color === "white" ? styles.pickDay : styles.pickDayAnimation
+              }
+              src={pickDay}
+            ></img>
+            <img
+              className={
+                color === "black" ? styles.pickNight : styles.pickNightAnimation
+              }
+              src={pickNight}
+            ></img>
+          </div>
         </div>
-        <div className={styles.icon}>
-        <div className={styles.whiteGrad}></div>
-         
+
+        <div className={ color === "black" ? styles.containerBtn : styles.containerBtnDay } onClick={(e) => handleClickBurguer(e)}>
+        <div className={`${styles.btn} ${btn}`} >
+          <span></span>
+          <span></span>
+          <span></span>  
         </div>
       </div>
+      
+
+        <div className={styles.icon}>
+          <div className={styles.whiteGrad}></div>
+        </div>
+      </div>
+     
+      <div className={ nav }>
+        <ul>
+          <li>Blog</li>
+          <li>Courses</li>
+          <li>Discord</li>
+          <li>Chats</li>
+          <li>Calls</li>
+          <li>Workshops</li>
+          <li>About</li>
+        </ul>
+      </div>
+      
+      
     </div>
   );
 }
