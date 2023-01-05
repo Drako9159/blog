3d6c3cb178cb52baf42d329b790937836e1bc226
drako9159 <drako9159@gmail.com>
2023-01-05 01:06:12 -0600

    solved bug icon day night

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Layout from "./components/Layout";
 import Wrapper from "./components/Wrapper";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Start from "./views/start/Start";
 import About from "./views/About";
 import Blog from "./views/Blog";
 
 function App() {
+ 
+  /**
+   * Set theme day default
+   */
+  localStorage.getItem("theme") === null ? localStorage.setItem("theme", "day") : "";
+
+  
   const [color, setColor] = useState(localStorage.getItem("theme"));
 
   return (
@@ -18,8 +25,14 @@ function App() {
               path="/"
               element={<Start color={color} setColor={setColor} />}
             ></Route>
-            <Route path="/about" element={<About color={color} setColor={setColor} />}></Route>
-            <Route path="/blog" element={<Blog color={color} setColor={setColor} />}></Route>
+            <Route
+              path="/about"
+              element={<About color={color} setColor={setColor} />}
+            ></Route>
+            <Route
+              path="/blog"
+              element={<Blog color={color} setColor={setColor} />}
+            ></Route>
           </Routes>
         </BrowserRouter>
       </Wrapper>
