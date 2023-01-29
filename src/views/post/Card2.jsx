0f98631ba570dbe9a4258dc2ac73d55110dfcf01
drako9159 <drako9159@gmail.com>
2023-01-28 19:11:07 -0600

    configure color conditional

--- src/views/post/Card2.jsx
+++ src/views/post/Card2.jsx
@@ -2,12 +2,18 @@ import styles from "./Card2.module.css";
 import ChargeAnimation from "../../components/ChargeAnimation";
 
 export default function Card1({ color, language, post, status }) {
-
-  if(post.length === 0){
-    return <ChargeAnimation color={color}></ChargeAnimation>
+  if (post.length === 0) {
+    return <ChargeAnimation color={color}></ChargeAnimation>;
   }
-  if(status === 400){
-    return <div className={styles.container}>NOT POST FOUND</div>
+  if (status === 400) {
+    return (
+      <div
+        style={color === "night" ? { color: "#ffffff" } : { color: "#000000" }}
+        className={styles.container}
+      >
+        NOT POST FOUND
+      </div>
+    );
   }
   return (
     <div
