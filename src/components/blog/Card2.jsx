c44b499d9541c601d0f8e13a94068a6d38ffbe81
drako9159 <drako9159@gmail.com>
2023-02-06 13:42:41 -0600

    solved bug images

--- src/components/blog/Card2.jsx
+++ src/components/blog/Card2.jsx
@@ -16,7 +16,6 @@ export default function Card2({ posts, status }) {
   if (status >= 400) {
     return <NotRequest status={status} />;
   }
-  
 
   return (
     <div className={styles.container}>
@@ -26,7 +25,7 @@ export default function Card2({ posts, status }) {
             <div className={styles.card}>
               <div className={styles.head}>
                 <img
-                  src={`https://backendblog.fly.dev/api/v2/images/articles${e.image}`}
+                  src={`http://localhost:3000/api/v2/images/articles${e.image}`}
                   alt={e.title}
                   className={styles.pick}
                   style={{ outlineColor: `${e.color}` }}
